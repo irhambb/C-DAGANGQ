1d549da29372baaf4e90d095d45eb4c9a83334af
Dea Ananda Dwi Pertiwi <57389079+deaanandapp@users.noreply.github.com>
2019-12-02 13:29:42 +0700

    Deskripsi Kedai

---
+++ src/app/deskripsikedai.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deskripsikedai',
+  templateUrl: './deskripsikedai.page.html',
+  styleUrls: ['./deskripsikedai.page.scss'],
+})
+export class DeskripsikedaiPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
